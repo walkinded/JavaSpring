217d8a8a1aa373d57b64546f9649f6b772315a23
WalkinDed <labunskij1997@gmail.com>
2021-01-23 16:41:08 +0300

    Поменялась структуру сайта Теперь на главной странице показывает статьи Убран URL "blog"

--- src/main/java/com/testsite/blog/controllers/BlogController.java
+++ src/main/java/com/testsite/blog/controllers/BlogController.java
@@ -21,11 +21,12 @@ public class BlogController {
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping("/blog")
+    @GetMapping("/")
     public String blogMain(Model model) {
+        model.addAttribute("title", "Главная страница");
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
-        return "blog-main";
+        return "home";
     }
 
     @GetMapping("/blog/add")
@@ -35,13 +36,13 @@ public class BlogController {
     public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
-        return "redirect:/blog";
+        return "redirect:/";
     }
 
-    @GetMapping("/blog/{id}")
+    @GetMapping("/{id}")
     public String blogDetails(@PathVariable(value = "id") long postId, Model model) {
         if(!postRepository.existsById(postId)) {
-            return "redirect:/blog";
+            return "redirect:/";
         }
         Optional<Post> post = postRepository.findById(postId);
         ArrayList<Post> result = new ArrayList<>();
@@ -50,10 +51,10 @@ public class BlogController {
         return "blog-details";
     }
 
-    @GetMapping("/blog/{id}/edit")
+    @GetMapping("/{id}/edit")
     public String blogEdit(@PathVariable(value = "id") long postId, Model model) {
         if(!postRepository.existsById(postId)) {
-            return "redirect:/blog";
+            return "redirect:/";
         }
         Optional<Post> post = postRepository.findById(postId);
         ArrayList<Post> result = new ArrayList<>();
@@ -62,21 +63,21 @@ public class BlogController {
         return "blog-edit";
     }
 
-    @PostMapping("/blog/{id}/edit")
+    @PostMapping("/{id}/edit")
     public String blogPostUpdate(@PathVariable(value = "id") long postId, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
         Post post = postRepository.findById(postId).orElseThrow();
         post.setTitle(title);
         post.setAnons(anons);
         post.setFull_text(full_text);
         postRepository.save(post);
-        return "redirect:/blog";
+        return "redirect:/";
     }
 
     @PostMapping("/blog/{id}/remove")
     public String blogPostDelete(@PathVariable(value = "id") long postId, Model model) {
         Post post = postRepository.findById(postId).orElseThrow();
         postRepository.delete(post);
-        return "redirect:/blog";
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/testsite/blog/controllers/MainController.java
+++ src/main/java/com/testsite/blog/controllers/MainController.java
@@ -3,16 +3,10 @@ package com.testsite.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("title", "Главная страница");
-        return "home";
-    }
 
     @GetMapping("/about")
     public String about(Model model) {
